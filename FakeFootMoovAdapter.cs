ec7e539b26dad493e6add51275f9dae3871b829f
4lbertoC <4lbertoc@gmail.com>
2014-03-18 08:26:03 +0100

    Added a fake adapter, to test the listener with the keyboard instead of the real shoe

---
+++ FakeFootMoovAdapter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace FootMoovTeam {
+	
+	public class FakeFootMoovAdapter : MonoBehaviour {
+
+		public MorseFootMoovListener listener;
+		private event OnFrontTapEvent OnFrontTap;
+		private event OnBackTapEvent OnBackTap;
+
+		void Awake() {
+			OnBackTap += new OnBackTapEvent (listener.OnBackTap);
+			OnFrontTap += new OnFrontTapEvent (listener.OnFrontTap);
+		}
+
+		void Update () {
+			
+			UpdateBackTap ();
+			UpdateFrontTap ();
+
+		}
+		
+		private void UpdateBackTap() {
+			if (Input.GetKeyUp (KeyCode.B)) {
+				OnBackTap (this);
+			}
+		}
+		
+		private void UpdateFrontTap() {
+			if (Input.GetKeyUp (KeyCode.F)) {
+				OnFrontTap (this);
+			}
+		}
+	}
+}
